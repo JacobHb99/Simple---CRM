2d5c828a6d962264a0e5a534bd915a0ce9a7f28a
JacobHb99 <jacob.hb@live.de>
2023-12-31 14:05:40 +0100

    ordersComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
 import { Component } from '@angular/core';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { OrdersComponent } from './orders/orders.component';
 
 export const routes: Routes = [
     {path: '', component: DashboardComponent},
     {path: 'user', component: UserComponent},
     {path: 'user/:id', component: UserDetailComponent},
-    {path: 'dashboard', component: DashboardComponent}
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'orders', component: OrdersComponent}
 ];
---
+++ src/app/orders/orders.component.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+import { UserComponent } from '../user/user.component';
+import { Firestore, collection, onSnapshot, query } from '@angular/fire/firestore';
+import { MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-orders',
+  standalone: true,
+  imports: [],
+  templateUrl: './orders.component.html',
+  styleUrl: './orders.component.scss'
+})
+export class OrdersComponent {
+  allUsers = Array();
+  allOrders = Array();
+
+
+
+
+  constructor(public dialog: MatDialog, private firestore: Firestore) {
+  }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+
+  async getUsers() {
+    const userCollection = collection(this.firestore, 'users');
+    const q = query(userCollection);
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      this.allUsers = [];
+      this.allOrders = [];
+      snapshot.forEach((doc) => {
+        this.pushOrders(doc.data());
+      });
+    });
+  }
+
+
+  pushOrders(user: any) {
+    for (let i = 0; i < user.orders.length; i++) {
+      const order = user.orders[i];
+      order.user = `${user.firstName} ${user.lastName}`;
+      this.allOrders.push(order);
+
+    }
+    console.log(this.allOrders); 
+  }
+
+
+  getOrderDate(timeStamp :number) {
+    let date = new Date(timeStamp);
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    return `${day}.${month}.${year}`;    
+}
+
+
+checkOrderAsDone(order: any, $index: any) {
+  this.allOrders[$index].status = 'done';  
+}
+
+}
